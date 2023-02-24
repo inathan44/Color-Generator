2847c3e558990add90932717bfa0c86766acb377
inathan44 <inathan44@yahoo.com>
2023-02-23 21:32:08 -0700

    Added html for example site

--- src/ExampleSite.tsx
+++ src/ExampleSite.tsx
@@ -3,7 +3,33 @@
 // Background = #e4faee
 
 const ExampleSite = () => {
-  return <div>ExampleSite</div>;
+  return (
+    <div>
+      <nav className="flex justify-end">
+        <h1 className="mr-auto">Kernel</h1>
+        <ul className="flex">
+          <li>About</li>
+          <li>Careers</li>
+          <li>Blogs</li>
+          <li>Follow</li>
+          <li>Try for free</li>
+        </ul>
+      </nav>
+      <main>
+        <section>
+          <div className="flex flex-col items-center">
+            <h1>Your content</h1>
+            <h1>copilot</h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio
+              dolore ut
+            </p>
+            <button>Try for free</button>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default ExampleSite;
